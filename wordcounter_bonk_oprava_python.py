bcee6757d3b151184474ef878afc244f8b5c6973
bonkpr00 <60886624+bonkpr00@users.noreply.github.com>
2020-04-27 10:15:18 +0200

    Update wordcounter_bonk_oprava_python.py

--- wordcounter_bonk_oprava_python.py
+++ wordcounter_bonk_oprava_python.py
@@ -1,23 +1,22 @@
 import urllib.request
 import sys
-import argparse
-
 
 stranka = urllib.request.urlopen('https://www.novinky.cz/koronavirus/clanek/vedci-ve-wu-chanu-delali-s-koronavirem-absolutni-silenosti-tvrdi-predni-rusky-mikrobiolog-40321795')
 obsah_stranky = stranka.read()
 obsah_stranky_dekodovany = obsah_stranky.decode('utf-8')
 stranka.close()
 
-znaky = len(obsah_stranky)
-print (f"Počet znaků: {znaky}")
-slova = slova = len(obsah_stranky_dekodovany.split(" ")) + len (obsah_stranky_dekodovany.split ('\n')) - 1
-print (f"Počet slov: {slova}")
-radky = len(obsah_stranky_dekodovany.split("\n"))
-print (f"Počet řádků: {radky}")
-
-
-
-
-
-
+try:
+    znaky = len(obsah_stranky_dekodovany)
+    print (f"Počet znaků: {znaky}")
+    slova = len(obsah_stranky_dekodovany.split(" ")) + len (obsah_stranky_dekodovany.split ('\n')) - 1
+    print (f"Počet slov: {slova}")
+    radky = len(obsah_stranky_dekodovany.split("\n"))
+    print (f"Počet řádků: {radky}")
+except FileNotFoundError:
+    print("STRÁNKA NEEXISTUJE")
+    sys.exit()
+except PermissionError:
+    print("PŘÍSTUP ODEPŘEN")
+    sys.exit()
 
